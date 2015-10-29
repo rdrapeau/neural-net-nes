6739951768df30a9a43e0e49b187d056e98cb00d
Aaron Nech <aaronnech@gmail.com>
2015-10-29 13:18:38 -0700

    fixed

--- bin/server/main.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="../common/def/express.d.ts"/>
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname + '/../client/static'));
-app.listen(1337);
-console.log('Listening on port 1337...');
--- bin/vendor/README.md
+++
@@ -1,4 +0,0 @@
-Vendor Files
-------------
-
-Vendor files are JavaScript that are not available as NPM packages, and thus must be included manually
