e63aae5d6f6c98119ac4da34fc816f1f519abba1
Aaron Nech <aaronnech@gmail.com>
2015-11-08 17:55:59 -0800

    fixed

---
+++ src/client/adapter/FlappyAdapter.ts
@@ -0,0 +1,42 @@
+import FlappySimulator = require('../flappybird/FlappySimulator');
+
+class FlappyAdapter {
+    public stateSize = 1; // Size of the state vector passed to the NN
+    public numActions = 2; // Number of possible actions from the NN
+    public waitTime = 0; // Time in ms to wait after performing an action to get the next state
+    public numIterations = 10000; // Number of training samples to use before stopping (not games)
+    public brain = null; // a reference to the brain
+    public onTrainingDone; // A function to call when the brain is done training and wants to test
+
+    private sim: FlappySimulator;
+
+    constructor(simulation: FlappySimulator, onTrainingDone) {
+        this.sim = simulation;
+        this.onTrainingDone = onTrainingDone;
+    }
+
+    public onGameStart() {
+        this.sim.onAction(1);
+        this.brain.onGameStart();
+    }
+
+    public getGameState() {
+        return this.sim.onGetState();
+    }
+
+    public getReward(gameState, action, newState) {
+        // Returns the reward given the game state, the action performed, and the new state
+        return action ? 1 : -1;
+    }
+
+    public onAction(action) {
+        this.sim.onAction(action);
+    }
+
+    public onDoneTrain(json) {
+        console.log(json);
+        this.onTrainingDone();
+    }
+}
+
+export = FlappyAdapter;
