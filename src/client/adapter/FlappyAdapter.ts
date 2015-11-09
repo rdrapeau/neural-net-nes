7da40d70cc649a430128a3fe0a91c6d003722db5
Ryan Drapeau <drapeau@cs.washington.edu>
2015-11-08 23:58:51 -0800

    mroe fix

--- src/client/adapter/FlappyAdapter.ts
+++ src/client/adapter/FlappyAdapter.ts
@@ -1,9 +1,8 @@
 import FlappySimulator = require('../flappybird/FlappySimulator');
 
 class FlappyAdapter {
-    public stateSize = 1; // Size of the state vector passed to the NN
+    public stateSize = 2; // Size of the state vector passed to the NN
     public numActions = 2; // Number of possible actions from the NN
-    public numIterations = 10000; // Number of training samples to use before stopping (not games)
     public brain = null; // a reference to the brain
 
     private sim: FlappySimulator;
@@ -29,7 +28,6 @@ class FlappyAdapter {
     public getReward(gameState, action, newState) {
         // Returns the reward given the game state, the action performed, and the new state
         if (newState.dead) {
-            console.log('dead');
             return -1000;
         }
 
