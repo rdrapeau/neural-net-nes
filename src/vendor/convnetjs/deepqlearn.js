e5860773bb28c88bd74a257d409b17dcf10d8aca
Ryan Drapeau <drapeau@cs.washington.edu>
2015-11-08 18:06:02 -0800

    no logging

--- src/vendor/convnetjs/deepqlearn.js
+++ src/vendor/convnetjs/deepqlearn.js
@@ -5,7 +5,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
 
 (function(global) {
   "use strict";
-  
+
   // An agent is in state0 and does action0
   // environment then assigns reward0 and provides new state, state1
   // Experience nodes store all this information, which is used in the
@@ -25,14 +25,14 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
     // in number of time steps, of temporal memory
     // the ACTUAL input to the net will be (x,a) temporal_window times, and followed by current x
     // so to have no information from previous time step going into value function, set to 0.
-    this.temporal_window = typeof opt.temporal_window !== 'undefined' ? opt.temporal_window : 1; 
+    this.temporal_window = typeof opt.temporal_window !== 'undefined' ? opt.temporal_window : 1;
     // size of experience replay memory
     this.experience_size = typeof opt.experience_size !== 'undefined' ? opt.experience_size : 30000;
     // number of examples in experience replay memory before we begin learning
-    this.start_learn_threshold = typeof opt.start_learn_threshold !== 'undefined'? opt.start_learn_threshold : Math.floor(Math.min(this.experience_size*0.1, 1000)); 
+    this.start_learn_threshold = typeof opt.start_learn_threshold !== 'undefined'? opt.start_learn_threshold : Math.floor(Math.min(this.experience_size*0.1, 1000));
     // gamma is a crucial parameter that controls how much plan-ahead the agent does. In [0,1]
     this.gamma = typeof opt.gamma !== 'undefined' ? opt.gamma : 0.8;
-    
+
     // number of steps we will learn for
     this.learning_steps_total = typeof opt.learning_steps_total !== 'undefined' ? opt.learning_steps_total : 100000;
     // how many steps of the above to perform only random actions (in the beginning)?
@@ -41,7 +41,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
     this.epsilon_min = typeof opt.epsilon_min !== 'undefined' ? opt.epsilon_min : 0.05;
     // what epsilon to use at test time? (i.e. when learning is disabled)
     this.epsilon_test_time = typeof opt.epsilon_test_time !== 'undefined' ? opt.epsilon_test_time : 0.01;
-    
+
     // advanced feature. Sometimes a random action should be biased towards some values
     // for example in flappy bird, we may want to choose to not flap more often
     if(typeof opt.random_action_distribution !== 'undefined') {
@@ -56,7 +56,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
     } else {
       this.random_action_distribution = [];
     }
-    
+
     // states that go into neural net to predict optimal action look as
     // x0,a0,x1,a1,x2,a2,...xt
     // this variable controls the size of that temporal window. Actions are
@@ -69,7 +69,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
     this.action_window = new Array(this.window_size);
     this.reward_window = new Array(this.window_size);
     this.net_window = new Array(this.window_size);
-    
+
     // create [state -> value of all possible actions] modeling net for the value function
     var layer_defs = [];
     if(typeof opt.layer_defs !== 'undefined') {
@@ -100,17 +100,17 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
     }
     this.value_net = new convnetjs.Net();
     this.value_net.makeLayers(layer_defs);
-    
+
     // and finally we need a Temporal Difference Learning trainer!
     var tdtrainer_options = {learning_rate:0.01, momentum:0.0, batch_size:64, l2_decay:0.01};
     if(typeof opt.tdtrainer_options !== 'undefined') {
       tdtrainer_options = opt.tdtrainer_options; // allow user to overwrite this
     }
     this.tdtrainer = new convnetjs.SGDTrainer(this.value_net, tdtrainer_options);
-    
+
     // experience replay
     this.experience = [];
-    
+
     // various housekeeping variables
     this.age = 0; // incremented every backward()
     this.forward_passes = 0; // incremented every forward()
@@ -124,7 +124,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
   Brain.prototype = {
     random_action: function() {
       // a bit of a helper function. It returns a random action
-      // we are abstracting this away because in future we may want to 
+      // we are abstracting this away because in future we may want to
       // do more sophisticated things. For example some actions could be more
       // or less likely at "rest"/default state.
       if(this.random_action_distribution.length === 0) {
@@ -145,7 +145,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
       var svol = new convnetjs.Vol(1, 1, this.net_inputs);
       svol.w = s;
       var action_values = this.value_net.forward(svol);
-      var maxk = 0; 
+      var maxk = 0;
       var maxval = action_values.w[0];
       for(var k=1;k<this.num_actions;k++) {
         if(action_values.w[k] > maxval) { maxk = k; maxval = action_values.w[k]; }
@@ -153,12 +153,12 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
       return {action:maxk, value:maxval};
     },
     getNetInput: function(xt) {
-      // return s = (x,a,x,a,x,a,xt) state vector. 
+      // return s = (x,a,x,a,x,a,xt) state vector.
       // It's a concatenation of last window_size (x,a) pairs and current state x
       var w = [];
       w = w.concat(xt); // start with current state
       // and now go backwards and append states and actions from history temporal_window times
-      var n = this.window_size; 
+      var n = this.window_size;
       for(var k=0;k<this.temporal_window;k++) {
         // state
         w = w.concat(this.state_window[n-1-k]);
@@ -175,7 +175,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
       // compute forward (behavior) pass given the input neuron signals from body
       this.forward_passes += 1;
       this.last_input_array = input_array; // back this up
-      
+
       // create network input
       var action;
       if(this.forward_passes > this.temporal_window) {
@@ -183,12 +183,12 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
         var net_input = this.getNetInput(input_array);
         if(this.learning) {
           // compute epsilon for the epsilon-greedy policy
-          this.epsilon = Math.min(1.0, Math.max(this.epsilon_min, 1.0-(this.age - this.learning_steps_burnin)/(this.learning_steps_total - this.learning_steps_burnin))); 
+          this.epsilon = Math.min(1.0, Math.max(this.epsilon_min, 1.0-(this.age - this.learning_steps_burnin)/(this.learning_steps_total - this.learning_steps_burnin)));
         } else {
           this.epsilon = this.epsilon_test_time; // use test-time value
         }
         var rf = convnetjs.randf(0,1);
-        console.log("epsilon: " + this.epsilon);
+        // console.log("epsilon: " + this.epsilon);
         if(rf < this.epsilon) {
           // choose a random action with epsilon probability
           action = this.random_action();
@@ -199,20 +199,20 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
           console.log("POLICY ACTION " + action);
        }
       } else {
-        // pathological case that happens first few iterations 
+        // pathological case that happens first few iterations
         // before we accumulate window_size inputs
         var net_input = [];
         action = this.random_action();
       }
-      
+
       // remember the state and action we took for backward pass
       this.net_window.shift();
       this.net_window.push(net_input);
-      this.state_window.shift(); 
+      this.state_window.shift();
       this.state_window.push(input_array);
-      this.action_window.shift(); 
+      this.action_window.shift();
       this.action_window.push(action);
-      
+
       return action;
     },
     backward: function(reward) {
@@ -220,12 +220,12 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
       this.average_reward_window.add(reward);
       this.reward_window.shift();
       this.reward_window.push(reward);
-      
-      if(!this.learning) { return; } 
-      
+
+      if(!this.learning) { return; }
+
       // various book-keeping
       this.age += 1;
-      
+
       // it is time t+1 and we have to store (s_t, a_t, r_t, s_{t+1}) as new experience
       // (given that an appropriate number of state measurements already exist, of course)
       if(this.forward_passes > this.temporal_window + 1) {
@@ -243,7 +243,7 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
           this.experience[ri] = e;
         }
       }
-      
+
       // learn based on experience, once we have some samples to go on
       // this is where the magic happens...
       if(this.experience.length > this.start_learn_threshold) {
@@ -265,10 +265,10 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
     },
     visSelf: function(elt) {
       elt.innerHTML = ''; // erase elt first
-      
+
       // elt is a DOM element that this function fills with brain-related information
       var brainvis = document.createElement('div');
-      
+
       // basic information
       var desc = document.createElement('div');
       var t = '';
@@ -279,11 +279,11 @@ var deepqlearn = deepqlearn || { REVISION: 'ALPHA' };
       t += 'smooth-ish reward: ' + this.average_reward_window.get_average() + '<br />';
       desc.innerHTML = t;
       brainvis.appendChild(desc);
-      
+
       elt.appendChild(brainvis);
     }
   }
-  
+
   global.Brain = Brain;
 })(deepqlearn);
 
